71b47cfb6a1b3e4e9fbc3636c4f38bbafc04e0ec
Chris Stanton <cstanton0760@yahoo.com>
2017-08-01 00:59:09 -0500

    added alerts from alertify.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,22 +9,71 @@ var waypoint = new Waypoint({
   offset: 100
 });
 
-function logAlerts() {
-  var logOption = document.getElementById('alertifyDotJS-logs').value;
-  console.log(logOption);
-  if (logOption === 'log'){
-    alertify.log("alertify.js: Standard log message");
-  } else if(logOption === 'success') {
-    alertify.success("alertify.js: Standard success message");
-  } else if(logOption === 'error') {
-    alertify.error("alertify.js: Standard errormessage");
-  } else {
-    alert('make a selection');
-    return
-  }
-};// end logAlerts()
+  function alertifyDotJSAlertLogs() {
+    var logOption = document.getElementById('alertifyDotJS-logs').value;
+    console.log(logOption);
+    if (logOption === 'log'){
+      alertify.log("alertify.js: Standard log message");
+    } else if (logOption === 'success') {
+      alertify.success("alertify.js: Standard success message");
+    } else if (logOption === 'error') {
+      alertify.error("alertify.js: Standard errormessage");
+    } else if (logOption === 'click-close'){
+      alertify
+        .closeLogOnClick(true)
+        .log("Click me to close!")
+    } else if (logOption === 'click-disabled'){
+      alertify
+        .closeLogOnClick(true)
+        .log("Click me to close!")
+        .closeLogOnClick(false)
+        .log("You can't click to close this!")
+    } else if (logOption === 'hide'){
+      alertify.delay(10000).log("Hiding in 10 seconds");
+    } else {
+      alert('make a selection');
+      return
+    };
+  };// end logAlerts()
 
-
+  function alertifyDotJSAlerts() {
+    var alertsOption = document.getElementById('alertifyDotJS-alerts').value;
+    if (alertsOption === 'dialog'){
+      alertify.alert("Dialog Alert");
+    } else if (alertsOption === 'confirm'){
+      alertify.confirm("Confirm Dialog", function () {
+      // user clicked "ok"
+      }, function() {
+      // user clicked "cancel"
+      });
+    } else if (alertsOption === 'prompt'){
+      alertify
+        .defaultValue("Default Value")
+        .prompt("Prompt Dialog",
+        function (val, ev) {
+          // The click event is in the event variable, so you can use it here.
+          ev.preventDefault();
+          // The value entered is availble in the val variable.
+          alertify.success("You've clicked OK and typed: " + val);
+        }, function(ev) {
+          // The click event is in the event variable, so you can use it here.
+          ev.preventDefault();
+          alertify.error("You've clicked Cancel");
+        });
+    } else if (alertsOption === 'promise-aware'){
+      if ("function" !== typeof Promise) {
+        alertify.alert("Your browser doesn't support promises");
+        return;
+      };
+        alertify.confirm("Confirm?").then(function (resolvedValue) {
+        resolvedValue.event.preventDefault();
+        alertify.alert("You clicked the " + resolvedValue.buttonClicked + " button!");
+        });
+    } else {
+      alertify.alert("make a selection");
+      return
+    };
+  };// end alertifyDotJSAlerts()
 
 
 
